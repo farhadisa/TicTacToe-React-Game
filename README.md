4b048ffa3ece170b39d59efff47af047b4adc71d
farhadisa <farhad.isa@yahoo.com>
2024-10-15 17:17:57 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Tic Tac Toe game with React
+
+Tic Tac Toe
+
+HTML
+
+CSS 
+
+React
+
+This is a Tic Tac Toe game, that is created with React by me in 2024.
+
---
+++ src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setplayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editOrSave) => !editOrSave);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setplayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        ) : (
+          <span classnames="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
